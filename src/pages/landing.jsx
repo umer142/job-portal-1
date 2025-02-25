72ec8f956e735f687d23bbc345962377755ce9ea
umer142 <umersjafiq79@gmail.com>
2025-02-25 05:08:41 +0500

    first commit

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -21,14 +21,9 @@ const LandingPage = () => {
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
       <section className="text-center ">
         <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
-          Find Your Dream Job
+          Find Your Dream Job With
           <span className="flex items-center gap-2 sm:gap-6">
-            and get
-            <img
-              src="/logo.png"
-              className="h-14 sm:h-24 lg:h-32"
-              alt="Hirrd Logo"
-            />
+            Emperi Staffing
           </span>
         </h1>
         <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
@@ -68,7 +63,39 @@ const LandingPage = () => {
         </CarouselContent>
       </Carousel>
 
-      <img src="/banner.jpeg" className="w-full" />
+      <div className="flex justify-center items-center w-[100vw] h-[80vh]">
+        <article className="w-[80vw] h-[70vh] bg-gray-800 shadow p-4 space-y-2 rounded-md hover:-translate-y-2 duration-300">
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            height="24"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            className="w-10 h-10 text-gray-300 bg-gray-600 rounded-full p-1"
+          >
+            <path
+              d="m8.032 12 1.984 1.984 4.96-4.96m4.55 5.272.893-.893a1.984 1.984 0 0 0 0-2.806l-.893-.893a1.984 1.984 0 0 1-.581-1.403V7.04a1.984 1.984 0 0 0-1.984-1.984h-1.262a1.983 1.983 0 0 1-1.403-.581l-.893-.893a1.984 1.984 0 0 0-2.806 0l-.893.893a1.984 1.984 0 0 1-1.403.581H7.04A1.984 1.984 0 0 0 5.055 7.04v1.262c0 .527-.209 1.031-.581 1.403l-.893.893a1.984 1.984 0 0 0 0 2.806l.893.893c.372.372.581.876.581 1.403v1.262a1.984 1.984 0 0 0 1.984 1.984h1.262c.527 0 1.031.209 1.403.581l.893.893a1.984 1.984 0 0 0 2.806 0l.893-.893a1.985 1.985 0 0 1 1.403-.581h1.262a1.984 1.984 0 0 0 1.984-1.984V15.7c0-.527.209-1.031.581-1.403Z"
+              strokeWidth="2"
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              stroke="currentColor"
+            ></path>
+          </svg>
+          <h1 className="text-center font-extrabold font-serif text-4xl">
+            Blogs
+          </h1>
+          <p className="text-sm w-full text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            eget massa interdum, rhoncus ex dignissim, ultricies tellus. Proin
+            convallis mauris ut est pulvinar aliquet. Nulla facilisi. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Nisi unde
+            necessitatibus veritatis eius labore! Saepe incidunt laboriosam
+            corrupti minus magni soluta, voluptate cupiditate, ratione nostrum
+            aliquam rem tempore earum aut! Corporis obcaecati molestiae saepe!
+          </p>
+        </article>
+      </div>
 
       <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <Card>
